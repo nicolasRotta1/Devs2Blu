17f10e698a87845493721a4cefa985cc8798a0af
Nícolas Rotta <142179878+nicolasRotta1@users.noreply.github.com>
2025-06-30 13:53:28 -0300

    Update ContaBancaria.java

--- AulaPOO_27_06/src/ContaBancaria.java
+++ AulaPOO_27_06/src/ContaBancaria.java
@@ -13,15 +13,27 @@ public class ContaBancaria {
         this.numero = new Random().nextInt(1000, 10000);
     }
 
+    public void menu(Scanner sc){
+        System.out.println("----------------- Conta Bancaria -----------------");
+        System.out.println("Bem vindo "+this.titular)
+        while(true){
+            System.out.println("-------------------- MENU -------------------- ");
+            System.out.println("(1) - Mostrar Conta - \n(2) - Depositar - \n(3) - Sacar - \n(0) - Sair -  ")
+            int opc = sc.nextInt();
+            switch case (opc) {
+                case 1: mostrarConta();
+                        default;
+        }
+    }
     public void cadastrar(Scanner sc){
         System.out.println("-- Cadastro de conta bancaria --\nDigite o nome de quem ira utilizar a conta: ");
         this.titular = sc.next();
 
     }
     public void mostrarConta(){
-        System.out.println("Número da conta: "+numero);
-        System.out.println("Número da conta: "+titular);
-        System.out.println("Número da conta: "+saldo);
+        System.out.println("Número da conta: "+this.numero);
+        System.out.println("Número da conta: "+this.titular);
+        System.out.println("Número da conta: "+this.saldo);
     }
     public void Sacar(){
         this.saldo-= qtd;
