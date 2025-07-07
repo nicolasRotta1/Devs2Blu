4004d6fb4224171e2d6e7caffbbd7bdc824cb302
Nícolas Rotta <142179878+nicolasRotta1@users.noreply.github.com>
2025-07-07 13:59:40 -0300

    Atualizar o script01.js

--- Módulo 02 - HTML, CSS e JS/Aula 04/Exercicios/01/script01.js
+++ Módulo 02 - HTML, CSS e JS/Aula 04/Exercicios/01/script01.js
@@ -1,25 +1,14 @@
- const calcular = () =>{
+function calcularMedia(n1, n2, n3) {
+    const media = (n1 + n2 + n3) / 3;
+    let situacao = '';
 
-            let formulario = document.getElementById('formulario');
-            let campos = document.getElementsByTagName('input');
+    if (media >= 7) {
+        situacao = 'Aprovado';
+    } else if (media >= 5) {
+        situacao = 'Recuperação';
+    } else {
+        situacao = 'Reprovado';
+    }
 
-            let nota1 = parseFloat(formulario.nota1.value);
-            let nota2 = parseFloat(formulario.nota2.value);
-            let nota3 = parseFloat(formulario.nota3.value);
-
-            let media = (nota1 + nota2 + nota3) / 3;
-
-            let status = media >= 7 ? 'Aprovado' : (media >= 5 ? 'recuperacao' : 'Reprovado');
-            let cor = media >= 7 ? 'green' : (media >= 5 ? 'orange' : 'red');
-
-             let retornaMedia = document.getElementById('retornaMedia');
-             let retornaStatus = document.getElementById('retornaStatus');
-
-            retornaMedia.innerText = `Média: ${media.toFixed(2)}`;
-            retornaStatus.innerText = `Situação: ${status}`;
-
-            
-            retornaStatus.style.color = cor;
-            return false;
-        }
-  
+    return { media: media.toFixed(2), situacao };
+}
