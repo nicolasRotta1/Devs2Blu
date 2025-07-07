0641ef6b10afa8088f803cced2ad29e90c3457c8
Nícolas Rotta <142179878+nicolasRotta1@users.noreply.github.com>
2025-07-07 14:02:13 -0300

    apenas javascript

--- Módulo 02 - HTML, CSS e JS/Aula 04/Exercicios/03/script03.js
+++ Módulo 02 - HTML, CSS e JS/Aula 04/Exercicios/03/script03.js
@@ -1,33 +1,23 @@
-const geradorNumero = Math.floor(Math.random() * 100) + 1;
-const verificar = () => {
-const numeroInput = document.getElementById('Numero');
-const  numero = parseInt(numeroInput.value);
+function jogoAdivinhacao(chutes) {
+    const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    let tentativas = 0;
 
-const  elementoFeedback = document.getElementById('feedback');
+    for (let chute of chutes) {
+        tentativas++;
+        const diferenca = Math.abs(numeroSecreto - chute);
 
-document.getElementById('tentativas').textContent = `Tentativas: ${contadorTentativas}`; 
-document.getElementById('feedback').textContent = '';
-
-let diferenca = Math.abs(geradorNumero - numero);
-
-
-if (isNaN(numero) || numero < 1 || numero > 100) {
-    elementoFeedback.textContent = 'Por favor, digite um número válido entre 1 e 100.';
-
-}
-
-if (diferenca === 0) {
-        elementoFeedback.textContent = `Parabéns, você acertou! O número era ${geradorNumero} em ${contadorTentativas} tentativas!`;
-        alert('Você acertou! O jogo será reiniciado.'); 
-        return false
-    } else if (diferenca <= 5) {
-        elementoFeedback.textContent = 'Quase!!';
-    } else if (diferenca <= 10) {
-        elementoFeedback.textContent = 'Perto!';
-    } else if (diferenca <= 20) {
-        elementoFeedback.textContent = 'Longe!';
-    } else {
-        elementoFeedback.textContent = 'Muito longe!';
+        if (chute === numeroSecreto) {
+            return `Acertou em ${tentativas} tentativas!`;
+        } else if (diferenca <= 5) {
+            console.log("Quase!");
+        } else if (diferenca <= 10) {
+            console.log("Perto!");
+        } else if (diferenca <= 20) {
+            console.log("Longe!");
+        } else {
+            console.log("Muito longe!");
+        }
     }
-   return false; 
+
+    return `Não acertou. O número era ${numeroSecreto}.`;
 }
