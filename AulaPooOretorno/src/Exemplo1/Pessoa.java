7041aadca9d085291b291e8f96956120d9ffd226
nicolasRotta1 <nrotta79@gmail.com>
2025-07-22 22:12:38 -0300

    Atualizando repositorio

---
+++ AulaPooOretorno/src/Exemplo1/Pessoa.java
@@ -0,0 +1,18 @@
+package Exemplo1;
+
+public class Pessoa {
+    public int id;
+    public String nome;
+    public int idade;
+    public double altura;
+
+    public Pessoa(){
+
+    }
+    public Pessoa(int id, String nome, int idade, double altura){
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+}
---
+++ AulaPooOretorno/src/Exemplo2/Peixe.java
@@ -0,0 +1,19 @@
+package Exemplo2;
+
+public class Peixe {
+    public int id;
+    public String especie;
+    public double peso;
+    public double tamanho;
+
+    public Peixe(){
+
+    }
+
+    public Peixe(int id, String especie, double peso, double tamanho) {
+        this.id = id;
+        this.especie = especie;
+        this.peso = peso;
+        this.tamanho = tamanho;
+    }
+}
